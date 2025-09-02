bfd100dd9eee4ba84ee57c6ece45bebee5bf3f0c
kemp-blake <kemp.m.blake@gmail.com>
2025-09-02 10:24:30 +0100

    Session 2

---
+++ Session 2/ses2/src/main/java/ses2/Main.java
@@ -0,0 +1,40 @@
+package ses2;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        //Scanner scanner = new Scanner(System.in);
+//
+//
+//
+        //// This won't give a gamer tag
+        //System.out.println("Enter your name: ");
+        //String name = scanner.nextLine();
+        //System.out.println("Enter your age: ");
+        //int age = scanner.nextInt();
+        //System.out.println("Enter your score: ");
+        //double score = scanner.nextDouble();
+        //System.out.println("Enter your Game Tag: ");
+        //String gamertag = scanner.nextLine();
+        //System.out.println("Hello "+ name +"! Your age is "+ age +" and your score is "+score+". Your Game Tag is: "+ gamertag);
+//
+        ////This is with parsing the next lines which should work.
+        //
+        //System.out.println("Enter your name: ");
+        //String name2 = scanner.nextLine();
+        //System.out.println("Enter your age: ");
+        //int age2 = Integer.parseInt(scanner.nextLine().trim());
+        //System.out.println("Enter your score: ");
+        //double score2 = Double.parseDouble(scanner.nextLine().trim());
+        //System.out.println("Enter your Game Tag: ");
+        //String gamertag2 = scanner.nextLine();
+        //System.out.println("Hello "+ name2 +"! Your age is "+ age2 +" and your score is "+score2+". Your Game Tag is: "+ gamertag2);
+//
+        //scanner.close();
+
+        //printf
+        System.out.printf("Integer: %d%nString: %s", 10, "test");
+        
+    }
+}
