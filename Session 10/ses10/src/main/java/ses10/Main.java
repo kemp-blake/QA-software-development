148520c340012306d59c5b49a23c3ce3ee1bb16d
kemp-blake <kemp.m.blake@gmail.com>
2025-09-12 11:22:30 +0100

    Session 10: threads

--- Session 10/ses10/src/main/java/ses10/Main.java
+++ Session 10/ses10/src/main/java/ses10/Main.java
@@ -1,7 +1,41 @@
 package ses10;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws InterruptedException {
+        Thread daemon = new Thread(() -> {
+            System.out.println("[DAEMON] This is from the Daemon after starting.");
+            while (true) {
+                try{
+                    Thread.sleep(00);
+                    System.out.println("[DAEMON] This is from the Daemon after waiting.");
+                } catch (InterruptedException e) {
+                    System.out.println("[DAEMON] [ERROR] - "+e);
+                }
+            }
+        });
+
+        daemon.setDaemon(true);
+        daemon.start();
+
+        Thread t1 = new Thread() {
+            @Override
+            public void run() {
+                System.out.println("This is from the T1 thread.");
+            }
+        };
+        //implementation of runnable interface
+        Runnable task = () -> System.out.println("This is from the T2 thread.");
+
+        Thread t2 = new Thread(task);
+
+        t1.start();
+        t2.start();
+
+        t1.join();
+        t2.join();
+        
+        System.out.println("[MAIN] - all threads finished");
+
+        
     }
 }
